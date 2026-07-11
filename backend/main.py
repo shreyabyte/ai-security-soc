a82109e60e1383c82bc5f1633c7079deb5fe95f1
Shreya Gupta <shreya.gupta2326@gmail.com>
2026-07-11 21:03:18 +0530

    cleared main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -6,10 +7,22 @@ import models
 import schemas
 from database import engine, SessionLocal
 
+# Create database tables
 models.Base.metadata.create_all(bind=engine)
 
+# Create FastAPI app
 app = FastAPI(title="AI SOC Backend")
 
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Later to be replaced "*" with Vercel frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Database session
 def get_db():
     db = SessionLocal()
     try:
@@ -17,6 +30,7 @@ def get_db():
     finally:
         db.close()
 
+# Create a new log
 @app.post("/logs", response_model=schemas.LogOut)
 def create_log(log: schemas.LogCreate, db: Session = Depends(get_db)):
     db_log = models.Log(**log.dict())
@@ -25,6 +39,7 @@ def create_log(log: schemas.LogCreate, db: Session = Depends(get_db)):
     db.refresh(db_log)
     return db_log
 
+# Get all logs
 @app.get("/logs", response_model=List[schemas.LogOut])
 def get_logs(db: Session = Depends(get_db)):
     return db.query(models.Log).order_by(models.Log.timestamp.desc()).all()
